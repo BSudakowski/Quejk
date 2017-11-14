3c36fb7bd794bfea56699aff787281a4a46bf521
BSudakowski <bsudakowski@gmail.com>
2017-11-14 17:01:51 +0100

    addFav

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,3 +2,14 @@ $(function(){
     // Activate mobile nav toggle button
     $(".button-collapse").sideNav({edge: 'right'});
 });
+
+$("#mark").click(function () {
+    if($("#mark").attr("class")=='mark favorite'){
+        $("#mark").removeClass("mark favourite");
+        $("#mark").addClass("unmark favourite");
+    }
+    else{
+        $("#mark").removeClass("unmark favourite");
+        $("#mark").addClass("mark favourite")
+    }
+});
